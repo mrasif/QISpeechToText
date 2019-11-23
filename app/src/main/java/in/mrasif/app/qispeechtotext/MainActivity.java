19253d029ed91e4682545b1fef73351673536b3d
Asif Mohammad Mollah <admin@mrasif.in>
2019-11-23 21:34:25 +0530

    Initial commit

---
+++ app/src/main/java/in/mrasif/app/qispeechtotext/MainActivity.java
@@ -0,0 +1,14 @@
+package in.mrasif.app.qispeechtotext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
